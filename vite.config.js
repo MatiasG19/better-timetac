cc025336ced71672deb5212beee334fe1bd778f8
MatiasG19 <m.galusek@gmail.com>
2024-05-26 12:47:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'better-timetac',
       fileName: 'main',
